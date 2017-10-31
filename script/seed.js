b020e6e2511ce1dbdf9f4c1f977cce9a68aa8605
mtamahori <mtamahori@gmaigmail.com>
2017-10-31 11:05:06 -0500

    chore: complete seed file

--- script/seed.js
+++ script/seed.js
@@ -34,13 +34,11 @@ const fakeUserGenerator = () => {
 const fakeProductGenerator = () => {
   const product = {
     releaseTitle: faker.lorem.words(),
-    // artist names aren't seeding as an array of strings
-    artists: [faker.name.firstName()],
+    artists: [faker.name.firstName() + ' ' + faker.name.lastName()],
     genre: fakeGenres[randomInt(0, 4)],
     releaseYear: randomInt(1920, 2017),
     imageUrl: faker.internet.url(),
-    // songInfo JSON format isn't perfect
-    songsInfo: [{ title: faker.fake('{{lorem.words}}') }, { duration: (randomInt(1,9).toString() + ":" + randomInt(0,9).toString() + randomInt(0,9).toString()) }],
+    songsInfo: [{ title: faker.fake('{{lorem.words}}'), duration: (randomInt(1,9).toString() + ":" + randomInt(0,9).toString() + randomInt(0,9).toString()) }],
     label: fakeLabels[randomInt(0, 4)],
     inventory: randomInt(0, 100),
     tags: [fakeTags[randomInt(0, 3)], fakeTags[randomInt(0, 3)]],
@@ -61,7 +59,6 @@ const fakeReviewGenerator = () => {
 
 const fakeOrderGenerator = () => {
   const order = {
-    // items JSON format isn't perfect
     items: [fakeProducts[randomInt(0, 49)], fakeProducts[randomInt(0, 49)]],
     sessionId: faker.random.number().toString(),
     hasShipped: false,
@@ -88,7 +85,7 @@ const createFakes = () => {
 
 createFakes();
 
-for (let i = 0; i < 20; i++) {
+for (let i = 0; i < 40; i++) {
   fakeReviewGenerator();
 }
 
@@ -129,57 +126,3 @@ const main = () => {
 };
 
 main();
-
-// BOILERMAKER BUILT-IN SEED FILE //
-
-// /**
-//  * Welcome to the seed file! This seed file uses a newer language feature called...
-//  *
-//  *                  -=-= ASYNC...AWAIT -=-=
-//  *
-//  * Async-await is a joy to use! Read more about it in the MDN docs:
-//  *
-//  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/async_function
-//  *
-//  * Now that you've got the main idea, check it out in practice below!
-//  */
-// const db = require('../server/db')
-// const {User} = require('../server/db/models')
-
-// async function seed () {
-//   await db.sync({force: true})
-//   console.log('db synced!')
-//   // Whoa! Because we `await` the promise that db.sync returns, the next line will not be
-//   // executed until that promise resolves!
-
-//   const users = await Promise.all([
-//     User.create({email: 'cody@email.com', password: '123'}),
-//     User.create({email: 'murphy@email.com', password: '123'})
-//   ])
-//   // Wowzers! We can even `await` on the right-hand side of the assignment operator
-//   // and store the result that the promise resolves to in a variable! This is nice!
-//   console.log(`seeded ${users.length} users`)
-//   console.log(`seeded successfully`)
-// }
-
-// // Execute the `seed` function
-// // `Async` functions always return a promise, so we can use `catch` to handle any errors
-// // that might occur inside of `seed`
-// seed()
-//   .catch(err => {
-//     console.error(err.message)
-//     console.error(err.stack)
-//     process.exitCode = 1
-//   })
-//   .then(() => {
-//     console.log('closing db connection')
-//     db.close()
-//     console.log('db connection closed')
-//   })
-
-// /*
-//  * note: everything outside of the async function is totally synchronous
-//  * The console.log below will occur before any of the logs that occur inside
-//  * of the async function
-//  */
-// console.log('seeding...')
