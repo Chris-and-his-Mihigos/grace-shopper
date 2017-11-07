146dc82e755d68d1bb7323b75e5e974ad7b3dc20
Christian Sadi <christiansadi@gmail.com>
2017-11-06 18:12:52 -0600

    'feat: update seed file and user home page'

--- script/seed.js
+++ script/seed.js
@@ -10,7 +10,13 @@ const fakeUsers = [];
 const fakeProducts = [];
 const fakeReviews = [];
 const fakeOrders = [];
-
+const fakeStatus = [
+  'cart',
+  'purchased',
+  'shipped',
+  'arrived',
+  'cancelled',
+]
 const fakeGenres = ['rock', 'jazz', 'hip-hop', 'classical', 'electronic'];
 const fakeLabels = ['Atlantic', 'Blue Note', 'Def Jam', 'Deutsche Grammophon', 'Warp']
 const fakeTags = ['chill', 'upbeat', 'deep', 'funky'];
@@ -32,28 +38,52 @@ const fakeImage = [
   faker.image.transport(),
 ]
 const fakeTitle = [
-  "No Discounts",
-  "Five-leaf Clover",
-  "Bursting Bubbles",
-  "Methodical Madness",
-  "All Ears",
-  "Chaos",
-  "Balls to the Wall",
-  "Here We Go",
-  "Once Bitten",
-  "Advantage",
-  "Form Over Function",
-  "Favoritism",
-  "Grains of Salt",
-  "Bare to the Bone",
-  "Badge of Honor",
-  "Forever Alone",
-  "Concept Art",
-  "Last Candidate",
-  "No Justice",
-  "Creative Director",
+  'No Discounts',
+  'Five-leaf Clover',
+  'Bursting Bubbles',
+  'Methodical Madness',
+  'All Ears',
+  'Chaos',
+  'Balls to the Wall',
+  'Here We Go',
+  'Once Bitten',
+  'Advantage',
+  'Form Over Function',
+  'Favoritism',
+  'Grains of Salt',
+  'Bare to the Bone',
+  'Badge of Honor',
+  'Forever Alone',
+  'Last Candidate',
+  'No Justice',
+  'Creative Director',
+  'Cherry',
+  'Feast of Nothing',
+  'Zero Gravity',
+  'The Last Laugh',
+  'Wild Goose Chase',
+  'Fight and Flight',
+  'Blue Moon',
+  'Unchained',
+  'Here We Go Again',
+  'Cloud Nine',
+  'Cat Napped',
+  'Blissful Ignorance',
+  'Death\'s Dead',
+  'Beggars and Thieves',
+  'Honesty',
+  'Concept Art',
+  'Crocodile Tears',
+  'Calm After the Storm',
+  'Battle Grounds',
+  'Fluke',
+  'Just a Tease',
+  'Basket Case',
+  'Cemetery of the Living',
+  'Bent Out of Shape',
+  'Function Over Form',
+  'Atmos',
 ]
-
 
 
 // RANDOM INTEGER GENERATOR (INCLUSIVE)
@@ -72,17 +102,16 @@ const fakeUserGenerator = () => {
   fakeUsers.push(user);
 }
 
-const releaseTitles = chance.unique(chance.word, 50);
+// const releaseTitles = chance.unique(chance.word, 50);
 
 const fakeProductGenerator = () => {
   const product = {
-    // releaseTitle: fakeTitle[randomInt(0, fakeTitle.length - 1)],
-    releaseTitle: releaseTitles.pop(),
-    artists: [`${faker.name.firstName()  } ${  faker.name.lastName()}`],
+    releaseTitle: fakeTitle.pop(),
+    artists: [`${faker.name.firstName()} ${faker.name.lastName()}`],
     genre: fakeGenres[randomInt(0, 4)],
     releaseYear: randomInt(1920, 2017),
     image: fakeImage[randomInt(0, 14)],
-    songsInfo: [{ title: faker.fake('{{lorem.words}}'), duration: (`${randomInt(1,9).toString()  }:${  randomInt(0,9).toString()  }${randomInt(0,9).toString()}`) }],
+    songsInfo: [{ title: faker.fake('{{lorem.words}}'), duration: (`${randomInt(1, 9).toString()}:${randomInt(0, 9).toString() }${randomInt(0, 9).toString()}`) }],
     label: fakeLabels[randomInt(0, 4)],
     inventory: randomInt(0, 100),
     price: randomInt(1, 50),
@@ -96,7 +125,7 @@ const fakeReviewGenerator = () => {
     text: 'Here is my review of this record. Much taste and forward thinking opinions.',
     rating: randomInt(0, 5),
     userId: randomInt(1, 20),
-    productId: randomInt(1, 50),
+    productId: randomInt(1, 40),
   }
   fakeReviews.push(review);
 }
@@ -104,8 +133,8 @@ const fakeReviewGenerator = () => {
 
 const fakeOrderGenerator = () => {
   const order = {
-    items: [fakeProducts[randomInt(0, 49)], fakeProducts[randomInt(0, 49)]],
-    status: 'cart',
+    items: [fakeProducts[randomInt(0, 39)], fakeProducts[randomInt(0, 39)]],
+    status: fakeStatus[randomInt(0, 4)],
     sessionId: faker.random.number().toString(),
     userId: randomInt(1, 20).toString(),
   }
@@ -117,10 +146,10 @@ const createFakes = () => {
   for (let i = 0; i < 20; i++) {
     fakeUserGenerator();
   }
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 40; i++) {
     fakeProductGenerator();
   }
-  for (let i = 0; i < 40; i++) {
+  for (let i = 0; i < 200; i++) {
     fakeOrderGenerator();
   }
 }
@@ -136,13 +165,13 @@ const seed = () =>
     User.create(user)))
     .then(() =>
       Promise.all(fakeProducts.map(product =>
-        Product.create(product)),))
+        Product.create(product)) ))
     .then(() =>
       Promise.all(fakeReviews.map(review =>
-        Review.create(review)),))
+        Review.create(review)) ))
     .then(() =>
       Promise.all(fakeOrders.map(order =>
-        Order.create(order)),));
+        Order.create(order)) ));
 
 const db = require('../server/db');
 
