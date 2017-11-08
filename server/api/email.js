0d296d38446d142318acb55f98ea2552e5e0633d
mtamahori <mtamahori@gmaigmail.com>
2017-11-07 20:21:29 -0600

    email functionality works

--- server/api/email.js
+++ server/api/email.js
@@ -1,113 +1,76 @@
-// const apiKey = '16c62cba4b06b2e507d9b35899def431-us17';
-// const listId = 'da74a128e9';
-// const mailURL = 'https://us17.api.mailchimp.com/3.0';
-
-// const request = require('superagent');
-
-// const Mailchimp = require('mailchimp-api-v3');
-// const mailchimp = new Mailchimp(apiKey);
-
 const router = require('express').Router();
+const Shipping = require('../db/models/shipping');
 
 module.exports = router
 
-const email = require('emailjs');
-const server = email.server.connect({
-  user: "persistentrecords@gmail.com",
-  password: "graceshopper",
-  host: "smtp.gmail.com",
-  port: 465,
-  ssl: true,
-});
+const nodemailer = require('nodemailer');
 
-router.post('/', (req, res, next) => {
-  server.send({
-    text: "Please work",
-    from: "PERSISTENT <persistentrecords@gmail.com>",
-    to: 'mtamahori@gmail.com',
-    cc: "Christian, <christiansadi@gmail.com>",
-    subject: "EMAILJS YO YO YO HELLO"
-  }, function (err, message) { console.log(err || message); })
-    .catch(next);
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'persistentrecords@gmail.com',
+    pass: 'graceshopper',
+  },
 })
 
-// router.use('/email', require('./email'));
+router.post('/purchase', (req, res, next) => {
+  const purchasedEmail = {
+    from: '"Persistent Records" <persistentrecords@gmail.com>',
+    to: `${req.body.email}`,
+    subject: 'Thank you!',
+    text: 'Thank you for your order. We will let you know when your shipment is on its way!',
+  };
 
-// export const purchaseOrder = (id, shipping, order) => (dispatch) => {
-//   axios.put(`/api/orders/${id}`, order)
-//     .then(res => dispatch(update(res.data)))
-//     .then(() => axios.put(`/api/orders/${id}/shipping`, shipping))
-//     .then(() => axios.post('/api/email', { email: shipping.email }))
-//     .then(() => dispatch(clearCart()))
-//     .catch(err => console.error(`Updating order: ${shipping} unsuccesful`, err))
-// };
+  transporter.sendMail(purchasedEmail, (error, info) => {
+    if (error) console.error(error);
+    console.log('Message sent: %s', info.messageId);
+  })
+})
 
+router.post('/update', (req, res, next) => {
 
+  let emailAddress = 'mtamahori@gmail.com';
 
+  const shippedEmail = {
+    from: '"Persistent Records" <persistentrecords@gmail.com>',
+    to: `${emailAddress}`,
+    subject: 'Order Shipped',
+    text: 'Your order is on its way!',
+  };
 
-// example email
-// const email = 'persistentrecords@gmail.com'
+  const deliveredEmail = {
+    from: '"Persistent Records" <persistentrecords@gmail.com>',
+    to: `${emailAddress}`,
+    subject: 'Woooo!',
+    text: 'Your order has been delivered!',
+  };
 
-// const md5 = require('md5');
+  const cancelledEmail = {
+    from: '"Persistent Records" <persistentrecords@gmail.com>',
+    to: `${emailAddress}`,
+    subject: 'Oh no!',
+    text: 'Your order has been cancelled!',
+  };
 
-// Send purchase confirmation email
+  let updateEmail;
 
-// router.post('/', (req, res, next) => {
-//   request
-//     .post('https://us17.api.mailchimp.com/3.0/lists/da74a128e9/members')
-//     .set('Content-Type', 'application/json;charset=utf-8')
-//     .set('Authorization', `Basic ${new Buffer('any:' + apiKey).toString('base64')}`)
-//     .send({
-//       email_address: req.body.email,
-//       status: 'subscribed',
-//     })
-//     .catch(next)
-// })
+  if (req.body.status === 'shipped') updateEmail = shippedEmail;
+  if (req.body.status === 'arrived') updateEmail = deliveredEmail;
+  if (req.body.status === 'cancelled') updateEmail = cancelledEmail;
 
+  Shipping.findOne({
+    where: {
+      orderId: req.body.id,
+    },
+  })
+    .then((shipping) => {
+      emailAddress = shipping.email;
+    })
 
-
-
-
-// router.get(`/lists${listId}/members/:${md5(email)}`, (req, res, next) => {
-
-// })
-
-// console.log('md5(email)', md5(email));
-
-// router.post(`/lists${listId}/members`, (req, res, next) => {
-
-// })
-
-//
-
-// mailchimp.get({
-//   path: `/lists${listId}/members`
-// })
-//   .then((members) => {
-//     console.log(members);
-//   })
-
-
-// POST request example:
-
-// {
-//   "name": "Persistent Records",
-//   "contact": {
-//     "company": "MailChimp",
-//     "address1": "675 Ponce De Leon Ave NE",
-//     "address2": "Suite 5000",
-//     "city": "Atlanta",
-//     "state": "GA",
-//     "zip": "30308",
-//     "country": "US",
-//     "phone": ""
-//   },
-//   "permission_reminder": "You're receiving this email so you are aware of your order's status!",
-//   "campaign_defaults": {
-//     "from_name": "Persistent Records",
-//     "from_email": "orders@persistentrecords.com",
-//     "subject": "Order Status Update",
-//     "language": "en"
-//   },
-//   "email_type_option": true
-// }
+  transporter.sendMail(updateEmail, (error, info) => {
+    if (error) console.error(error);
+    console.log('Message sent: %s', info.messageId);
+  });
+})
